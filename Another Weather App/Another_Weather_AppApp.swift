a28d0ff699c96a96cdfbaa49dc70429e1e92d978
Leon <mrtonmoy12@gmail.com>
2024-05-30 22:27:17 +0600

    Initial Commit

---
+++ Another Weather App/Another_Weather_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Another_Weather_AppApp.swift
+//  Another Weather App
+//
+//  Created by Mahbubur Rashid Leon on 2024-05-30.
+//
+
+import SwiftUI
+
+@main
+struct Another_Weather_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Another Weather App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Another Weather App
+//
+//  Created by Mahbubur Rashid Leon on 2024-05-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
