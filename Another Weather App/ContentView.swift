25379dd25e51710f90d5054c79179f6c55766e95
Leon <mrtonmoy12@gmail.com>
2024-06-02 12:47:51 +0600

    -- Primary UI structure is created

--- Another Weather App/ContentView.swift
+++ Another Weather App/ContentView.swift
@@ -8,17 +8,102 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isNight: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            BackgroundView(isNight: isNight)
+            
+            VStack {
+                CityTextView(cityName: "Dhaka")
+                
+                MainWeatherStatusView(isNight: $isNight)
+                
+                HStack(spacing: 20) {
+                    WeatherDayView(dayOfWeek: "SAT", imageName: "cloud.sun.rain.fill", temperature: 20)
+                    WeatherDayView(dayOfWeek: "SUN", imageName: "cloud.sun.bolt.fill", temperature: 30)
+                    WeatherDayView(dayOfWeek: "MON", imageName: "sun.haze.fill", temperature: 50)
+                    WeatherDayView(dayOfWeek: "TUE", imageName: "sun.snow.fill", temperature: 10)
+                    WeatherDayView(dayOfWeek: "WED", imageName: "snowflake", temperature: 20)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    isNight.toggle()
+                }, label: {
+                    WeatherButton(title: "Change Day Time", textColor: .blue, backgroundColor: .white)
+                })
+                
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 16, weight: .medium))
+                .foregroundStyle(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .frame(width: 40, height: 40)
+            
+            Text("\(temperature)°")
+                .font(.system(size: 28, weight: .medium))
+                .foregroundStyle(.white)
+        }
+    }
+}
+
+struct BackgroundView: View {
+    var isNight: Bool
+    
+    var body: some View {
+        LinearGradient(
+            colors: [isNight ? .black : .blue, isNight ? .gray : Color("LightBlue")],
+            startPoint: .topLeading, endPoint: .bottomTrailing)
+        .ignoresSafeArea()
+    }
+}
+
+struct CityTextView: View {
+    var cityName: String
+    
+    var body: some View {
+        Text("\(cityName), Bangladesh")
+            .font(.system(size: 30, weight: .medium))
+            .foregroundStyle(.white)
+            .padding()
+    }
+}
+
+struct MainWeatherStatusView: View {
+    @Binding var isNight: Bool
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            Image(systemName: isNight ? "moon.stars.fill" : "cloud.sun.fill")
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 180, height: 180)
+            
+            Text("\(isNight ? 10 : 40)°")
+                .font(.system(size: 70, weight: .medium))
+                .foregroundStyle(.white)
+        }.padding(.bottom, 40)
+    }
 }
---
+++ Another Weather App/WeatherButton.swift
@@ -0,0 +1,23 @@
+//
+//  WeatherButton.swift
+//  Another Weather App
+//
+//  Created by Mahbubur Rashid Leon on 2024-05-30.
+//
+
+import SwiftUI
+
+struct WeatherButton: View {
+    var title: String
+    var textColor: Color
+    var backgroundColor: Color
+    
+    var body: some View {
+        Text(title)
+            .frame(width: 280, height: 50)
+            .font(.system(size: 20, weight:.bold))
+            .foregroundStyle(textColor)
+            .background(backgroundColor)
+            .cornerRadius(10)
+    }
+}
